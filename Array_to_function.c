aaadf09a1bf0a801e74440c2c1b9c12c6cf787fc
☺ <61087995+Ayayaw@users.noreply.github.com>
2021-11-20 14:01:25 +0300

    Create Array_to_function.c

---
+++ Array_to_function.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+// Initialize functions
+void read_array(int**,int n);
+int find_small(int**, int n);
+
+int main(){
+	
+	// Initialize variables
+	int* num;
+	int n,smallest;
+	
+	// Size of the array
+	printf("\n Enter the size of the array : ");
+	scanf("%d",&n);
+	
+	// Memory allocation for array
+	num = (int*)malloc(n*sizeof(int));
+	
+	// Append value to array
+	read_array(&num,n);
+	
+	// Find smallest number in array
+	smallest = find_small(&num,n);
+	
+	// Display smallest number of array.
+	printf("\n The samllest number in the array is = %d", smallest);
+	getch();
+	return 0;
+}
+
+void read_array(int **arr, int n){
+	int i;
+	for(i = 0; i < n; i++){
+		printf("\n arr[%d] = ",i);
+		// Append element of arr[i]
+		scanf("%d",&arr[i]);
+	}
+}
+
+int find_small(int **arr,int n){
+	
+	int i = 0, small = *arr[0];
+	// Go through the array until you find the smallest value
+	for(i = 1; i < n; i++){
+		if(*arr[i]<small) small = *arr[i];
+		// When found make small variable equals to smallest number
+	}
+	return small;
+}
